62c09645e6d43d0df49f04822c8dc104529227dc
rmfls0606 <rmfls0606@naver.com>
2025-03-07 03:59:52 +0900

    [Feat] 탭바VC생성 후 이동 확인

---
+++ CheckBit/View/TabbarViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TabbarViewController.swift
+//  CheckBit
+//
+//  Created by 이상민 on 3/7/25.
+//
+
+import UIKit
+
+final class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+    }
+    
+    private func configureView() {
+        let firstVC = ViewController()
+        firstVC.tabBarItem = UITabBarItem(
+            title: "거래소",
+            image: UIImage(systemName: "chart.line.uptrend.xyaxis"),
+            tag: 0
+        )
+        
+        
+        let secondVC = ViewController()
+        secondVC.tabBarItem = UITabBarItem(
+            title: "코인정보",
+            image: UIImage(systemName: "chart.bar.fill"),
+            tag: 1
+        )
+        
+        let thirdVC = ViewController()
+        thirdVC.tabBarItem = UITabBarItem(
+            title: "포트폴리오",
+            image: UIImage(systemName: "star"),
+            selectedImage: UIImage(systemName: "star.fill")
+        )
+        thirdVC.tabBarItem.tag = 2
+        
+        
+        self.tabBar.tintColor = .black
+        self.viewControllers = [firstVC, secondVC, thirdVC]
+    }
+}
