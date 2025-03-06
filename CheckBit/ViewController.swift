f73b2788a905946fa09f3164b1ad328938ab1e13
rmfls0606 <rmfls0606@naver.com>
2025-03-06 16:34:16 +0900

    Initial Commit

---
+++ CheckBit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckBit
+//
+//  Created by 이상민 on 3/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
