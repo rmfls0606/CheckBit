0b60b464707ff3ccec99e81485253e566f40fad8
rmfls0606 <rmfls0606@naver.com>
2025-03-07 04:05:12 +0900

    [Rename, Add] 파일 구조화 작업 및 거래소VC 파일 생성

---
+++ CheckBit/View/Transaction/TransactionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TransactionViewController.swift
+//  CheckBit
+//
+//  Created by 이상민 on 3/7/25.
+//
+
+import UIKit
+
+class TransactionViewController: BaseViewController {
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+    override func configureView() {
+        self.view.backgroundColor = .orange
+    }
+    
+    override func configureBind() {
+        
+    }
+}
