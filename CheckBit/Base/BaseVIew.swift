64cb8d94fb664a2d5228782c70e748d47aa8ba18
rmfls0606 <rmfls0606@naver.com>
2025-03-08 01:39:49 +0900

    [Add] UIView의 뼈대가 되는 BaseVIew파일 생성

---
+++ CheckBit/Base/BaseVIew.swift
@@ -0,0 +1,29 @@
+//
+//  BaseVIew.swift
+//  CheckBit
+//
+//  Created by 이상민 on 3/8/25.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configureHierarchy(){ }
+    
+    func configureLayout(){ }
+    
+    func configureView(){ }
+}
