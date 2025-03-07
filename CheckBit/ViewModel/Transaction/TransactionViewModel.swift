495be6ed04cd24767c1c67952ca38a2e0ca83682
rmfls0606 <rmfls0606@naver.com>
2025-03-07 21:03:34 +0900

    [Add] TransactionViewModel 파일 생성

---
+++ CheckBit/ViewModel/Transaction/TransactionViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  TransactionViewModel.swift
+//  CheckBit
+//
+//  Created by 이상민 on 3/7/25.
+//
+
+import Foundation
+
+final class TransactionViewModel {
+    
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    func transform(input: Input) -> Output{
+        return Output()
+    }
+}
