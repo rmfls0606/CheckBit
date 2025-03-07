c6b32540d01b8004d42b47a91289c1e932a5b95d
rmfls0606 <rmfls0606@naver.com>
2025-03-07 21:36:54 +0900

    [Add] CoinTableView에서 사용할 CoinTableViewCell 파일 생성

---
+++ CheckBit/View/Transaction/CoinTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CoinTableViewCell.swift
+//  CheckBit
+//
+//  Created by 이상민 on 3/7/25.
+//
+
+import UIKit
+
+class CoinTableViewCell: BaseTableViewCell {
+    
+    static let identifier = "CoinTableViewCell"
+
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+
+}
