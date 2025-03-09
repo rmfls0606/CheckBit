52c4f011d55be31172e9b791ea2659ec7624f678
rmfls0606 <rmfls0606@naver.com>
2025-03-10 02:26:21 +0900

    [Feat] 코인을 검색할 TextField생성

--- CheckBit/View/CoinInformation/CoinInformationViewController.swift
+++ CheckBit/View/CoinInformation/CoinInformationViewController.swift
@@ -6,8 +6,38 @@
 //
 
 import UIKit
+import SnapKit
 
 class CoinInformationViewController: BaseViewController {
+    
+    
+    private lazy var textFieldBox: UIView = {
+        let view = UIView()
+        view.addSubview(textField)
+        view.layer.borderColor = UIColor(resource: .secondary).cgColor
+        view.layer.borderWidth = 1.0
+        return view
+    }()
+    
+    private let textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "검색어를 입력해주세요."
+        
+        let iconImageView = UIImageView(image: UIImage(systemName: "magnifyingglass"))
+        iconImageView.tintColor = UIColor(resource: .secondary)
+        textField.leftView = iconImageView
+        textField.leftViewMode = .always
+        
+        textField.backgroundColor = .white
+        textField.textColor = UIColor(resource: .secondary)
+        textField.font = UIFont.systemFont(ofSize: 16)
+        
+        textField.attributedPlaceholder = NSAttributedString(string: "검색어를 입력해주세요.", attributes: [.foregroundColor: UIColor(resource: .secondary)])
+        textField.returnKeyType = .search
+        
+        return textField
+    }()
+    
     //NavigationTitle
     private let navigationTitleLabel: UILabel = {
         let label = UILabel()
@@ -18,12 +48,22 @@ class CoinInformationViewController: BaseViewController {
         return label
     }()
     
+    override func viewDidLayoutSubviews() {
+        textFieldBox.layer.cornerRadius = textFieldBox.bounds.height / 2
+        textFieldBox.layer.masksToBounds = true
+    }
+    
     override func configureHierarchy() {
-        
+        self.view.addSubview(textFieldBox)
     }
     
     override func configureLayout() {
-        
+        self.textFieldBox.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(16)
+        }
+        self.textField.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(10)
+        }
     }
     
     override func configureView() {
