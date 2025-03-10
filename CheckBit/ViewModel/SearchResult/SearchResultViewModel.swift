51f262503da46e1ad6b6430459913001562a2c29
rmfls0606 <rmfls0606@naver.com>
2025-03-11 02:41:13 +0900

    [Add] SearchResultViewModel 파일 생성

---
+++ CheckBit/ViewModel/SearchResult/SearchResultViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultViewModel.swift
+//  CheckBit
+//
+//  Created by 이상민 on 3/11/25.
+//
+
+import Foundation
+
+class SearchResultViewModel {
+    struct Input{
+    
+    }
+    
+    struct Output{
+       
+    }
+    
+    func transform(input: Input) -> Output{
+     
+        
+        return Output()
+    }
+}
