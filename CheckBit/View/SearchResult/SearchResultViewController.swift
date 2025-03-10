2b197015c575de6c560848c381660c3098517c01
rmfls0606 <rmfls0606@naver.com>
2025-03-11 02:22:26 +0900

    [Feat] 검색 결과 화면 백 버튼 아이콘 변경

--- CheckBit/View/SearchResult/SearchResultViewController.swift
+++ CheckBit/View/SearchResult/SearchResultViewController.swift
@@ -32,6 +32,10 @@ class SearchResultViewController: BaseViewController {
     override func configureView() {
         self.view.backgroundColor = .white
         self.navigationItem.titleView = self.searchBar
+        self.navigationItem.hidesBackButton = true
+        let leftItem = UIBarButtonItem(image: UIImage(systemName: "arrow.left"), style: .done, target: self, action: nil)
+        leftItem.tintColor = UIColor(resource: .main)
+        self.navigationItem.leftBarButtonItem = leftItem
     }
     
     override func configureBind() {
