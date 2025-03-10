016f3aa5ea063cf0b745180393471e2f58bf43e2
rmfls0606 <rmfls0606@naver.com>
2025-03-10 17:28:45 +0900

    [Add] 기존에 작성되어 있던 화살표 방향 코드를 파일 생성하여 넣어주는 작업

---
+++ CheckBit/Model/ArrowImage.swift
@@ -0,0 +1,13 @@
+//
+//  ArrowImage.swift
+//  CheckBit
+//
+//  Created by 이상민 on 3/10/25.
+//
+
+import Foundation
+
+enum ArrowImage: String {
+    case up = "arrowtriangle.up.fill"
+    case down = "arrowtriangle.down.fill"
+}
