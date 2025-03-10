c9cd06784775ab7ad1bb737cf0a17b0ba8a306b7
rmfls0606 <rmfls0606@naver.com>
2025-03-10 17:02:17 +0900

    [Feat] 인기검색어 뷰모델에 api호출 함수 코드 작성

--- CheckBit/ViewModel/CoinInformation/CoinInformationViewModel.swift
+++ CheckBit/ViewModel/CoinInformation/CoinInformationViewModel.swift
@@ -6,20 +6,43 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 final class CoinInformationViewModel {
-    
+
+    private let disposeBag = DisposeBag()
+
     struct Input{
     
     }
     
     struct Output{
-    
+        let trendingList: PublishRelay<[TrendingCoinItem]>
     }
     
     func transform(input: Input) -> Output{
-       
-        return Output()
+        let trendingList = PublishRelay<[TrendingCoinItem]>()
+        
+        Observable<Int>.interval(.seconds(600), scheduler: MainScheduler.instance)
+            .startWith(0)
+            .flatMap{ _ in
+                NetworkManager.shared.callBackUpbitWithSingle(api: CoingeckoRequest.trending)
+                    .flatMap { (result: Result<TrendingCoins, Error>) -> Single<[TrendingCoinItem]> in
+                        switch result {
+                        case .success(let data):
+                            return Single.just(data.coins)
+                        case .failure(_):
+                            return Single.just([])
+                        }
+                    }
+            }
+            .subscribe { value in
+                trendingList.accept(value.element!)
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(trendingList: trendingList)
     }
 }
 
