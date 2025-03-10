9072053ed3033ed17afa8f95d9a749700c0e1e16
rmfls0606 <rmfls0606@naver.com>
2025-03-10 15:42:00 +0900

    [Fix] 헤더 아이템 선택이 없을 때 정렬 코드 수정

--- CheckBit/Model/MarketSort.swift
+++ CheckBit/Model/MarketSort.swift
@@ -39,11 +39,11 @@ enum SortItem{
         switch self{
         case .currentPrice(let option):
             return { coinList in
-                coinList.sorted{ option == .desc ? $0.trade_price > $1.trade_price : (option == .asc ? $0.trade_price < $1.trade_price : $0.acc_trade_price < $1.acc_trade_price) }
+                coinList.sorted{ option == .desc ? $0.trade_price > $1.trade_price : (option == .asc ? $0.trade_price < $1.trade_price : $0.acc_trade_price > $1.acc_trade_price) }
             }
         case .comparePreDay(let option):
             return { coinList in
-                coinList.sorted { option == .desc ? $0.signed_change_rate > $1.signed_change_rate : (option == .asc ? $0.signed_change_rate < $1.signed_change_rate : $0.acc_trade_price < $1.acc_trade_price) }
+                coinList.sorted { option == .desc ? $0.signed_change_rate > $1.signed_change_rate : (option == .asc ? $0.signed_change_rate < $1.signed_change_rate : $0.acc_trade_price > $1.acc_trade_price) }
             }
         case .tradePrice(let option):
             return { coinList in
