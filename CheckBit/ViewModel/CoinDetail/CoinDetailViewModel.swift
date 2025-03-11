040cb65ca96ceed4231d7159bf0e974b20e2d708
rmfls0606 <rmfls0606@naver.com>
2025-03-11 13:09:11 +0900

    [Feat] 상세화면에 필요한 데이터를 받아오기 위한 viewModel 생성

--- CheckBit/ViewModel/CoinDetail/CoinDetailViewModel.swift
+++ CheckBit/ViewModel/CoinDetail/CoinDetailViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class CoinDetailViewModel{
+    
+    struct Input{
+        let coinIds: Observable<String>
+    }
+    
+    struct Output{
+        let price_in_7d_list: PublishRelay<[CoinDetail]>
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output{
+        let price_in_7d_list = PublishRelay<[CoinDetail]>()
+        
+        input.coinIds
+            .flatMap { ids -> Single<[CoinDetail]> in
+                NetworkManager.shared
+                    .callBackUpbitWithSingle(api: CoingeckoRequest.coins(vs_currenct: "KRW", ids: ids))
+                    .flatMap { (
+                        result: Result<[CoinDetail],
+                        Error>
+                    ) -> Single<[CoinDetail]> in
+                        switch result{
+                        case .success(let data):
+                            return Single.just(data)
+                        case .failure(_):
+                            return Single.just([])
+                        }
+                    }
+            }
+            .subscribe { value in
+                price_in_7d_list.accept(value)
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(price_in_7d_list: price_in_7d_list)
+    }
+}
